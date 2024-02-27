637af2ea94e0ed7ab1d4ca6b4c9b6ac2c019f736
anakimdev <anatolich.work.english@gmail.com>
2024-02-27 17:26:40 +0300

    Create base HeadHunter data collector

---
+++ src/apies/hh/HHDataCollector.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+
+import requests
+
+class HHDataCollector:
+    def __init__(self, token:str, useragent:str):
+        self.__token = token
+        self.__useragent = useragent
+
+    @property
+    def token(self):
+        return self.__token
+
+    @token.setter
+    def token(self, token: str):
+        self.__token = token
+
+    @property
+    def useragent(self):
+        return self.__useragent
+
+    @useragent.setter
+    def useragent(self, new_agent: str):
+            self.__useragent = new_agent
+
+    def get_vacancies(self, data):
+        url = 'https://api.hh.ru/vacancies'
+        data = data
+        headers = {'Authorization': f'Bearer {self.token}',
+                    'User-Agent': self.useragent}
+        response = requests.get(url = url, data = data, headers = headers)
+        pprint(response.json())
+
