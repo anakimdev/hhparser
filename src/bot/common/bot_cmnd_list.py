629bcf50368464ce51afaad70c06949a29ff761d
anakimdev <anatolich.work.english@gmail.com>
2024-02-29 18:26:46 +0300

    Create list of allowed commands for users

---
+++ src/bot/common/bot_cmnd_list.py
@@ -0,0 +1,8 @@
+from aiogram.types import BotCommand
+
+
+commands_for_users = [
+    BotCommand(command='help', description='Узнать возможности бота'),
+    BotCommand(command='about', description='О нас'),
+    BotCommand(command='payment', description='Варианты оплаты'),
+]
