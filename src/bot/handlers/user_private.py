082318fad647eb9772f4cd75a286c99b19e76c03
anakimdev <anatolich.work.english@gmail.com>
2024-03-02 15:46:27 +0300

    Add filter for user_private handler

--- src/bot/handlers/user_private.py
+++ src/bot/handlers/user_private.py
@@ -1,12 +1,14 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.types import Message
 from aiogram.filters import Command
 
 from src.bot import configs
 from src.apies.main import create_data_collector
+from src.bot.filters.chat_types import ChatTypeFilter
 from src.bot.response_optimizer import optimization_result
 
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 data_collector = create_data_collector()
 
 @user_private_router.message(Command("start"))
@@ -25,12 +27,7 @@ async def help_handler(msg: Message):
 async def help_handler(msg: Message):
     await msg.answer("Варианты оплаты")
 
-
-
-
-
-
-@user_private_router.message()
+@user_private_router.message(F.text)
 async def it(msg: types.Message):
     profession = msg.text.lower().split()
     data = {
