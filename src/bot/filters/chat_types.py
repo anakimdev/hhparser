1d8861a15affd116c5eb808040bf36f3519fe2d3
anakimdev <anatolich.work.english@gmail.com>
2024-03-02 18:02:00 +0300

    Update filters for admins

--- src/bot/filters/chat_types.py
+++ src/bot/filters/chat_types.py
@@ -1,9 +1,17 @@
 from aiogram.filters import Filter
 from aiogram import types
 
+from src.bot.configs import ADMINS
+
+
 class ChatTypeFilter(Filter):
     def __init__(self, chat_types: list[str]) -> None:
         self.chat_types = chat_types
 
     async def __call__(self, message: types.Message) -> bool:
         return message.chat.type in self.chat_types
+
+
+class IsAdmin(Filter):
+    async def __call__(self, message: types.Message) -> bool:
+        return message.from_user.id in ADMINS
