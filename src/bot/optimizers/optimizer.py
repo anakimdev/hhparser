1dd778ec9576fad434f8fa16d62cc8f2a0046bc2
anakimdev <anatolich.work.english@gmail.com>
2024-03-11 17:47:19 +0300

    Refactor get_vacancies methods

---
+++ src/bot/optimizers/optimizer.py
@@ -0,0 +1,12 @@
+from src.apies.main import create_data_collector
+from src.bot.optimizers.response_optimizer import make_desc_vacancies, make_desc_vacancy, Vacancy, VacancyDesc
+
+collector = create_data_collector()
+
+
+async def get_vacancies(request_data: dict[str]) -> list[Vacancy]:
+    return make_desc_vacancies(await collector.collect_vacancies(request_data))
+
+
+async def get_vacancy(vacancy_id: str) -> VacancyDesc:
+    return make_desc_vacancy(await collector.collect_vacancy(vacancy_id))
