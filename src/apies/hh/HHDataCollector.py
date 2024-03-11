102715483ca3f5d3b7e2d32dc9558adf38d79f3e
anakimdev <anatolich.work.english@gmail.com>
2024-03-11 17:48:16 +0300

    Update HHDataCollector

--- src/apies/hh/HHDataCollector.py
+++ src/apies/hh/HHDataCollector.py
@@ -1,9 +1,8 @@
-from pprint import pprint
-
 import requests
 
+
 class HHDataCollector:
-    def __init__(self, token:str, useragent:str):
+    def __init__(self, token: str, useragent: str):
         self.__token = token
         self.__useragent = useragent
 
@@ -21,13 +20,19 @@ class HHDataCollector:
 
     @useragent.setter
     def useragent(self, new_agent: str):
-            self.__useragent = new_agent
+        self.__useragent = new_agent
 
-    def get_vacancies(self, data):
+    async def collect_vacancies(self, data):
         url = 'https://api.hh.ru/vacancies'
         data = data
-        headers = {'Authorization': f'Bearer {self.token}',
-                    'User-Agent': self.useragent}
+        headers = {'Authorization': f'Bearer {self.token}', 'User-Agent': self.useragent}
+
         response = requests.get(url = url, data = data, headers = headers)
         return response.json()
 
+    async def collect_vacancy(self, vacancy_id: str):
+        url = f'https://api.hh.ru/vacancies/{vacancy_id}'
+        headers = {'Authorization': f'Bearer {self.token}','User-Agent': self.useragent}
+
+        response = requests.get(url = url, headers = headers)
+        return response.json()
