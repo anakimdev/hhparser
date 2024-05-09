93bedfda1c9ad552faf79a7a6a9e63604959b6f4
anakimdev <anatolich.work.english@gmail.com>
2024-05-09 15:51:42 +0300

    Create base tests for company collectors

---
+++ tests/apies/company_info/InnDataCollector_test.py
@@ -0,0 +1,24 @@
+import unittest
+from src.apies.company_info.InnDataCollector import InnDataCollector
+
+
+class InnDataCollectorTest(unittest.TestCase):
+    '''Тесты для класса InnDataCollector'''
+
+    def setUp(self):
+        """Создание экземляра класса"""
+        self.inn_collector = InnDataCollector()
+
+    def test_collect_inn_data(self):
+        """Проверка на совпадение ИНН"""
+
+        dict_values = {'Тинькофф': '7710140679',
+                       'HeadHunter': '7718620740',
+                       'Сбербанк': '7707083893'}
+
+        for key, value in dict_values.items():
+            self.assertEqual(value, self.inn_collector.collect_inn(key))
+
+
+if __name__ == '__main__':
+    unittest.main()
