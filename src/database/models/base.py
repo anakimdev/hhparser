7b8239ab461678a78778fe32587beafd511bce5c
anakimdev <anatolich.work.space@gmail.com>
2024-06-05 00:34:09 +0300

    Create Base model for our ORM

---
+++ src/database/models/base.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import text, String
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+
+
+idpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
+str_256 = Annotated[str, mapped_column(String(256))]
+created_at = Annotated[datetime, mapped_column(server_default=text('TIMEZONE("utc", now())'))]
+updated_at = Annotated[datetime, mapped_column(
+    server_default=text('TIMEZONE("utc", now())'),
+    onupdate=datetime.utcnow())
+]
+
+
+class Base(DeclarativeBase):
+    repr_cols_num = 3
+    repr_cols = ()
+
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
+
