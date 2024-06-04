706b4c666fc9cfdf474dfa9881f18a3350836380
anakimdev <anatolich.work.space@gmail.com>
2024-06-05 00:48:07 +0300

    Create base version of IndustryOrm

---
+++ src/database/queries/industry_orm.py
@@ -0,0 +1,30 @@
+import json
+
+from database.core.engine import async_session
+from database.models.dictionaries import WorkloadTable, IndustryCategoryTable, IndustryTable
+from database.preprocessors.industries import preprocess_industries
+from database.preprocessors.industry_category import preprocess_industry_categories
+
+
+class IndustryOrm:
+    @staticmethod
+    async def insert_industry_categories():
+        categories = [IndustryCategoryTable(name=category) for category in preprocess_industry_categories()]
+
+        async with async_session() as session:
+            session.add_all(categories)
+            await session.commit()
+
+    @staticmethod
+    async def insert_industries():
+        async with async_session() as session:
+            data = preprocess_industries()
+            industries = []
+
+            for industry in data:
+                industries.append(IndustryTable(name=industry['name'], category_id=industry['category_id']))
+
+            session.add_all(industries)
+            await session.commit()
+
+
