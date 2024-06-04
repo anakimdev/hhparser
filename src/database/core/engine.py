11902880015c5f36d18d9226c503f5ad26ce599a
anakimdev <anatolich.work.space@gmail.com>
2024-06-05 00:33:22 +0300

    Create async engine for db

---
+++ src/database/core/engine.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from database.configs import settings
+
+async_engine = create_async_engine(
+    url = settings.database_url_asyncpg,
+    echo = True
+)
+
+async_session = async_sessionmaker(bind=async_engine)
