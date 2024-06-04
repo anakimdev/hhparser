63ecde2f45b50e7885496e47ecce74f4ef9824f9
anakimdev <anatolich.work.space@gmail.com>
2024-06-05 00:37:21 +0300

    Settings for our db

---
+++ src/database/configs.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    PG_ADMIN_EMAIL: str
+    PG_ADMIN_PASSWORD: str
+
+    @property
+    def database_url_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
