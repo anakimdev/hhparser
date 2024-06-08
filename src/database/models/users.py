8f01e36f32a5a7c0d398cd0004b30fa655f79630
anakimdev <anatolich.work.space@gmail.com>
2024-06-08 16:49:23 +0300

    Create model for user

---
+++ src/database/models/users.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+
+from database.models.common_dicts import RolesTable
+from src.database.models.base import Base, idpk, created_at, updated_at
+
+
+class UsersTable(Base):
+    __tablename__ = "users"
+
+    id: Mapped[idpk]
+    nickname: Mapped[str]
+    telegram_id: Mapped[int]
+    roles_id: Mapped[int] = mapped_column(ForeignKey('roles.id'))
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+    role: Mapped["RolesTable"] = relationship(
+        back_populates='user'
+    )
+
+    templates: Mapped[list["UserTemplatesTable"]] = relationship(
+        back_populates='user',
+    )
+
+
+class UserTemplatesTable(Base):
+    __tablename__ = "user_templates"
+
+    id: Mapped[idpk]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete="CASCADE"))
+    profession: Mapped[str]
+    expected_salary: Mapped[int]
+    region: Mapped[str]
+
+    user: Mapped["UserTable"] = relationship(
+        back_populates='templates'
+    )
+
+
