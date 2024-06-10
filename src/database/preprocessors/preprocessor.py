03a5bc0460103fba9437346e82418907cb886100
anakimdev <anatolich.work.space@gmail.com>
2024-06-11 01:36:46 +0300

    Update common Preprocessor handler

--- src/database/preprocessors/preprocessor.py
+++ src/database/preprocessors/preprocessor.py
@@ -1,31 +1,32 @@
-import json
-from abc import abstractmethod, ABC
+import asyncio
+from pprint import pprint
 
 from src.database.preprocessors.areas import AreaPreprocessor
+from src.database.preprocessors.configs import PROFESSIONS_FILE, VACANCIES_FILE
 from src.database.preprocessors.industries import IndustryPreprocessor, IndustryCategoryPreprocessor
+from src.database.preprocessors.ipreprocessor import IPreprocessor
 from src.database.preprocessors.professions import ProfessionPreprocessor, ProfessionCategoryPreprocessor
+from src.database.preprocessors.vacancies import VacancyPreprocessor
 
 strategies = {
     'area': AreaPreprocessor,
     'industry': IndustryPreprocessor,
     'industry_category': IndustryCategoryPreprocessor,
     'profession': ProfessionPreprocessor,
-    'profession_category': ProfessionCategoryPreprocessor
+    'profession_category': ProfessionCategoryPreprocessor,
+    'vacancy': VacancyPreprocessor
 }
 
-class IPreprocessor(ABC):
-    @abstractmethod
-    def preprocess(self, data):
-        pass
 
-
-class Preprocessor():
+class Preprocessor:
     @staticmethod
-    def preprocess(file_path:str, preprocessor:str):
+    async def preprocess(file_path: str, preprocessor: str):
         strategy = None
-        if isinstance(strategies.get(preprocessor), IPreprocessor):
+        if issubclass(strategies.get(preprocessor), IPreprocessor):
             strategy = strategies.get(preprocessor)
+        else:
+            raise ValueError('Unknown preprocessor')
 
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-        return strategy.preprocess(data)
+        result = await strategy.preprocess(file_path)
+        return result
+
