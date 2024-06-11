f1667c925937d198aae06a64d28844f3457b0f6d
anakimdev <anatolich.work.space@gmail.com>
2024-06-11 03:04:20 +0300

    Fix bug with datetime error in prepare_vacancies_data

--- src/database/preprocessors/vacancies.py
+++ src/database/preprocessors/vacancies.py
@@ -1,7 +1,8 @@
 import asyncio
-import itertools
 import json
 import os
+from datetime import datetime
+import pytz
 
 from src.database.preprocessors.ipreprocessor import IPreprocessor
 
@@ -20,24 +21,31 @@ class VacancyPreprocessor(IPreprocessor):
             tasks.append(task)
 
         await asyncio.gather(*tasks)
-        return common_list
+
+        vacancies = []
+        for item in common_list:
+            if item['api_id'] not in [x['api_id'] for x in vacancies]:
+                vacancies.append(item)
+
+        return vacancies
 
 
     @staticmethod
     async def _process_file(path: str):
         with open(f'{path}', 'r') as f:
             data = json.load(f)
-            data = [
+            results = [
                 {
                     **vacancy,
                     'api_id': int(vacancy['api_id']),
                     'area_id': int(vacancy['area_id']),
                     'professional_roles': [
                         {**role, 'id': int(role['id'])} for role in vacancy['professional_roles']
-                    ]
+                    ],
+                    'published_at': datetime.fromisoformat(vacancy['published_at']).astimezone(pytz.utc).replace(tzinfo=None)
                 } for vacancy in data
             ]
-            return data
+            return results
 
     @staticmethod
     async def __add_to_common_list(common_list, task_func, *args, **kwargs):
